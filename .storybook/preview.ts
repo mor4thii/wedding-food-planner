8c477f2e2d1090737a7e4f1e160d8073c68cb0b3
mor4thii <morathii@icloud.com>
2023-09-24 13:55:01 +0200

    ➕ Make Material UI and Storybook work together

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,19 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+
+/* TODO: update import for your custom Material UI themes */
+// import { lightTheme, darkTheme } from '../path/to/themes';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -10,6 +21,21 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    // Adds global styles and theme switching support.
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      // Uncomment for theme switching
+      // Provider: ThemeProvider,
+      // themes: {
+      // Provide your custom themes here
+      //   light: lightTheme,
+      //   dark: darkTheme,
+      // },
+      // defaultTheme: 'light',
+    }),
+  ],
 };
 
 export default preview;
